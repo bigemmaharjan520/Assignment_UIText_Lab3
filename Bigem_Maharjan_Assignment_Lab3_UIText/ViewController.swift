bde50a74809d3e98ae747fc19c5144d259255560
Bigem Maharjan <bigemmaharjan520@gmail.com>
2024-01-27 14:38:36 -0800

    First Commit

--- Bigem_Maharjan_Assignment_Lab3_UIText/ViewController.swift
+++ Bigem_Maharjan_Assignment_Lab3_UIText/ViewController.swift
@@ -9,6 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var FirstName: UITextField!
+    
+    
+    @IBOutlet var LastName: UITextField!
+    
+    
+    @IBOutlet var Country: UITextField!
+    
+    @IBOutlet var Age: UITextField!
+    
+    
+    @IBOutlet var AllOutput: UITextView!
+    
+    
+    
+    @IBOutlet var MessageLabel: UILabel!
+    
+    
+    @IBAction func AddUser(_ sender: Any) {
+    }
+    
+    
+    @IBAction func SubmitForm(_ sender: Any) {
+    }
+    
+    
+    @IBAction func ClearForm(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
