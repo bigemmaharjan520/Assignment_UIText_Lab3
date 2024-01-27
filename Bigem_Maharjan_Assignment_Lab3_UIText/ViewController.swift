4b208a856761b5c49b22c08336a91ce84f427237
Bigem Maharjan <bigemmaharjan520@gmail.com>
2024-01-27 09:00:48 -0800

    Initial Commit

---
+++ Bigem_Maharjan_Assignment_Lab3_UIText/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bigem_Maharjan_Assignment_Lab3_UIText
+//
+//  Created by user240741 on 1/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
